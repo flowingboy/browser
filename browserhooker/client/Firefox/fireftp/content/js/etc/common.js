572d4744b89b67a251741a495096c8d440548ca4
flowingboy <flowingboy@gmail.com>
2012-04-14 22:42:44 +0800

    update some files and delete some files.

--- browserhooker/client/Firefox/fireftp/content/js/etc/common.js
+++
@@ -1,3 +0,0 @@
-function $(el) {
-  return document.getElementById(el);
-}
--- browserhooker/client/Firefox/mozactivex-ff-15/activex.js
+++
@@ -1,6 +0,0 @@
-pref("general.useragent.vendorComment", "ax");
-pref("security.xpconnect.activex.global.hosting_flags", 9);
-pref("security.classID.allowByDefault", false);
-/* Windows Media Player */
-pref("capability.policy.default.ClassID.CID6BF52A52-394A-11D3-B153-00C04F79FAA6", "AllAccess");
-pref("capability.policy.default.ClassID.CID22D6F312-B0F6-11D0-94AB-0080C74C7E95", "AllAccess");
